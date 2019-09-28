928fee460d9e52da0557e17d5b54fa6b8e7d59e6
Jimmy Johansson <jim-joh@outlook.com>
2019-09-28 08:55:23 +0200

    update order wrong, working

--- main.js
+++ main.js
@@ -1,62 +1,70 @@
 // Ovning Webbutveckling .Net
 var inputs = document.getElementsByClassName("form-control")
-var wrapper = document.getElementById("wrapper")
+var parent = document.getElementById("wrapper")
 
-var idx = Date.now();
-
+/* Creates an blogg object and stringyfies it and 
+saves it to localstorage */
 function saveData() {
 
-    var idx = Date.now;
+    var idx = Date.now();
+    var bloggPost = { Id: idx, header: "", date: "", blogg: "" }
 
-    for (let i = 0; i < inputs.length; i++) {
-        switch (inputs[i].id) {
-            case header:
-                localStorage.setItem("header" + idx, inputs[i].innerHTML)
-                break;
+    bloggPost.header = inputs[0].value;
+    bloggPost.date = inputs[1].value;
+    bloggPost.blogg = inputs[2].value;
 
-            case date:
-                localStorage.setItem("date" + idx, inputs[i].innerHTML)
-                break;
+    localStorage.setItem(idx, JSON.stringify(bloggPost));
 
-            default:
-                localStorage.setItem("blogg" + idx, inputs[i].innerHTML)
-                break;
-        }
+    updateBlogg();
+}
+
+// Parses data from localstorage to json object and updates the view
+function updateBlogg() {
+    removeChilds()
+    for (let i = 0; i < localStorage.length; i++) {
+        var postObject = JSON.parse(localStorage.getItem(localStorage.key(i)));
+
+        appendElements(postObject);
     }
-    ++idx;
-
-    localStorage.setItem
-
 }
 
+// Appends graphical elements with data from json object
+function appendElements(postObject) {
 
-function parseData() {
-
-}
-
-function appendElements() {
-       
     var divContainer = document.createElement('div');
+    divContainer.setAttribute('class', "container");
     divContainer.setAttribute('id', "responseContainer");
 
     var article = document.createElement('article');
 
-    var pHead = document.createElement('p');
+    var pHead = document.createElement('h1');
     pHead.setAttribute('id', "header");
-    
+    pHead.innerHTML = postObject.header
+
     var pDate = document.createElement('p');
     pDate.setAttribute('id', "date");
-    
+    pDate.innerHTML = postObject.date
+
     var pText = document.createElement('p');
     pText.setAttribute('id', "bloggText");
+    pText.innerHTML = postObject.blogg
 
-    article.appendChild(pText)
-    article.appendChild(pDate)
     article.appendChild(pHead)
+    article.appendChild(pDate)
+    article.appendChild(pText)
     divContainer.appendChild(article)
-    wrapper.appendChild(divContainer);
+    parent.appendChild(divContainer);
+    parent.insertAdjacentElement("afterbegin", divContainer)
 
 }
 
+function clearPosts() {
+    localStorage.clear();
+    removeChilds()
+}
 
-// function
+function removeChilds() {
+    while (parent.hasChildNodes()) {
+        parent.removeChild(parent.lastChild)
+    }
+}
